16536429a6042269662a29e9b47cf3037d87b372
Bahadır Önal <bahadironal3@gmail.com>
2023-12-01 21:12:34 +0300

    controller update & admin view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
