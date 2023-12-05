f7fb07d2c987212aa1c2380dfcdfc3c0ec8388b2
Bahadır Önal <bahadironal3@gmail.com>
2023-12-05 13:31:50 +0300

    admin dashboard route update

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,3 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->middleware('auth', 'role:admin')->name('admin.dashboard');
