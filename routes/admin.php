f73438967503e2735e6fac32dacd219e206c9afc
Bahadır Önal <bahadironal3@gmail.com>
2024-02-07 07:32:52 +0300

    admin profile settings update

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
     Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+    //Profile Routes
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 
 });
