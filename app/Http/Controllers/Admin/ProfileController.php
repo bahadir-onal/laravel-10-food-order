127b3e6fef16485487a9e6a8fc1f8054cbc7f682
Bahadır Önal <bahadironal3@gmail.com>
2024-02-15 05:19:04 +0300

    admin profile function update

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
+use Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Profiler\Profile;
 
@@ -16,7 +17,15 @@ class ProfileController extends Controller
 
     function updateProfile(ProfileUpdateRequest $request)
     {
-        dd($request->all());
+        $user = Auth::user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        $user->save();
+
+        toastr('Updated Succesfully', 'success');
+
         return redirect()->back();
     }
 }
